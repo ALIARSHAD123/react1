f11454f3ed01e91fa9c270bb4eda86e202e9d46c
Ali Arshad <muhammedaliarshad788@example.com>
2026-04-29 14:53:47 +0500

    tut1

---
+++ my-first-project/src/App.jsx
@@ -0,0 +1,23 @@
+// function App() {
+//   return <h1>hu hahahah</h1>;
+// }
+
+// export default App;
+
+// import React from 'react '
+
+const App = () => {
+  return (
+    <div>
+      <div id="parent">
+        <h1>APP</h1>
+        <h4>ewrewr</h4>
+      </div>
+      <div id="child">
+        <h2>ahahahah</h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
