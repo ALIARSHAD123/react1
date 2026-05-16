2f047280b15d920a6ec37ab0e572e238630bfb68
Ali Arshad <muhammedaliarshad788@example.com>
2026-05-17 01:29:20 +0500

    api

--- 15-api-call/src/App.jsx
+++ 15-api-call/src/App.jsx
@@ -1,38 +1,35 @@
-import axios from 'axios'
-import  { useState } from 'react'
-
-
+import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
+  const [data, setdata] = useState([]);
 
+  const mygetData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
 
-  const [data, setdata] = useState([])
+    setdata(response.data);
+  };
 
-  const mygetData  = async()=>{
-
-    const response = await axios.get('https://picsum.photos/v2/list')
-     
-    setdata(response.data)
-
+  async function getDat() {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    console.log(response);
   }
 
- async function getDat(){
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    console.log(response)
-  }
+  const getData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  };
 
-  const getData = async ()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-    console.log(data)
-  }
+  const Data = async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    console.log(data);
+  };
 
-  const Data = async ()=>{
-    const {data} =await axios.get('https://jsonplaceholder.typicode.com/users')
-    console.log(data)
-
-  }
-  
   return (
     <div>
       <button onClick={getDat}>Get Dat</button>
@@ -40,15 +37,16 @@ const App = () => {
       <button onClick={Data}>Data</button>
       <button onClick={mygetData}>My Get Data</button>
       <div>
-        {data.map(function(elem , idx){
-          return <h3>aahhaahh, {elem.author} {idx} </h3>
-
+        {data.map(function (elem, idx) {
+          return (
+            <h3>
+              aahhaahh, {elem.author} {idx}{" "}
+            </h3>
+          );
         })}
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
